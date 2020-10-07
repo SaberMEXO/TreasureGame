bb1ab3172c3b9105822433f5403e39d56e187a2e
Yifan <ym2159@nyu.edu>
2020-10-07 09:33:28 -0700

    RayCastLayer fixed

--- Assets/sprites/RayCastWallCheck.cs
+++ Assets/sprites/RayCastWallCheck.cs
@@ -7,6 +7,8 @@ public class RayCastWallCheck : MonoBehaviour
     public float maxDistance = 2f;
     public float enemySpeed = 4f;
 
+    public LayerMask rayCastLayers;
+
     SpriteRenderer thisSprite;
 
     // Start is called before the first frame update
@@ -21,7 +23,7 @@ public class RayCastWallCheck : MonoBehaviour
         //define the ray
         Ray2D myRay = new Ray2D(transform.position, transform.up);
 
-        RaycastHit2D hit2D = Physics2D.Raycast(myRay.origin, myRay.direction, maxDistance);
+        RaycastHit2D hit2D = Physics2D.Raycast(myRay.origin, myRay.direction, maxDistance, rayCastLayers);
         //draw the ray
         Debug.DrawRay(myRay.origin, myRay.direction * maxDistance, Color.white);
 
